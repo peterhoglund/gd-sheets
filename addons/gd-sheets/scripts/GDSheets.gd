1a982d05c04bb616958555f8a7e8ff3c60dcbbbe
peterhoglund <peter.hoglund@gmail.com>
2022-02-24 21:58:50 +0100

    Delete addons/gd-sheets directory

--- addons/gd-sheets/scripts/GDSheets.gd
+++
@@ -1,9 +0,0 @@
-class_name GDSheets
-
-
-#GDSheets.sheet("Default")["Long Sword"]["Name"]
-static func sheet(sheet_name : String) -> Dictionary:
-	assert(load("res://addons/gd-sheets/documents/" + sheet_name + ".tres"), "[GodotSheets] No sheet called '" + sheet_name + "'.")
-	var sheet = load("res://addons/gd-sheets/documents/" + sheet_name + ".tres")
-	return sheet.hash_map
-
